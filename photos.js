b8beea676393f0ff6af1f48c94bc746c360d252c
Ivan1421 <shustik142@gmail.com>
2025-12-18 20:24:13 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -2,17 +2,17 @@
 // Используйте пути к файлам вместо Base64
 
 const playerPhotos = {
-    mbappe: "photos(2)/mbappe.jpg",
-    raya: "photos(2)/raya.jpg",
-    maradona: "photos(2)/maradona.jpg",
-    sanya: "photos(2)/sanya.jpg",
-    messi: "photos(2)/messi.jpg",
-    batrakov: "photos(2)/batrakov.jpg",
-    abibas: "photos(2)/abibas.jpg",
-    beloszhneka: "photos(2)/belosnezhka.jpg",
-    ramos: "photos(2)/ramos.jpg",
-    andryushka: "photos(2)/andryushka.jpg",
-    hokkeist: "photos(2)/hokkeist.jpg"
+    mbappe: "photos (2)/mbappe.jpg",
+    raya: "photos (2)/raya.jpg",
+    maradona: "photos (2)/maradona.jpg",
+    sanya: "photos (2)/sanya.jpg",
+    messi: "photos (2)/messi.jpg",
+    batrakov: "photos (2)/batrakov.jpg",
+    abibas: "photos (2)/abibas.jpg",
+    beloszhneka: "photos (2)/belosnezhka.jpg",
+    ramos: "photos (2)/ramos.jpg",
+    andryushka: "photos (2)/andryushka.jpg",
+    hokkeist: "photos (2)/hokkeist.jpg"
 };
 
 // ================================================================
