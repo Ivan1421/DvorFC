0433037897f5c964eaa1713947eb2a43ac692e25
Ivan1421 <shustik142@gmail.com>
2025-12-18 20:41:42 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,13 +1,5 @@
 const playerPhotos = {
-    mbappe: "/DvorFC/Photos (2)/mbappe.jpg",
-    raya: "/DvorFC/Photos (2)/raya.jpg",
-    maradona: "/DvorFC/Photos (2)/maradona.jpg",
-    sanya: "/DvorFC/Photos (2)/sanya.jpg",
-    messi: "/DvorFC/Photos (2)/messi.jpg",
-    batrakov: "/DvorFC/Photos (2)/batrakov.jpg",
-    abibas: "/DvorFC/Photos (2)/abibas.jpg",
-    beloszhneka: "/DvorFC/Photos (2)/belosnezhka.jpg",
-    ramos: "/DvorFC/Photos (2)/ramos.jpg",
-    andryushka: "/DvorFC/Photos (2)/andryushka.jpg",
-    hokkeist: "/DvorFC/Photos (2)/hokkeist.jpg"
+    mbappe: "https://ivan1421.github.io/DvorFC/Photos (2)/mbappe.jpg",
+    raya: "https://ivan1421.github.io/DvorFC/Photos (2)/raya.jpg",
+    // ... остальные по аналогии
 };
