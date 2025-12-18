b75502b09d1f12f88db833ba4de97d868f1ee1d6
Ivan1421 <shustik142@gmail.com>
2025-12-18 20:37:27 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,18 +1,13 @@
-// ==================== ССЫЛКИ НА ФОТО ИГРОКОВ ====================
-// Используйте пути к файлам вместо Base64
-
 const playerPhotos = {
-    mbappe: "Photos (2)/mbappe.jpg",
-    raya: "Photos (2)/raya.jpg",
-    maradona: "Photos (2)/maradona.jpg",
-    sanya: "Photos (2)/sanya.jpg",
-    messi: "Photos (2)/messi.jpg",
-    batrakov: "Photos (2)/batrakov.jpg",
-    abibas: "Photos (2)/abibas.jpg",
-    beloszhneka: "Photos (2)/belosnezhka.jpg",
-    ramos: "Photos (2)/ramos.jpg",
-    andryushka: "Photos (2)/andryushka.jpg",
-    hokkeist: Photos (2)/hokkeist.jpg
+    mbappe: "/DvorFC/Photos (2)/mbappe.jpg",
+    raya: "/DvorFC/Photos (2)/raya.jpg",
+    maradona: "/DvorFC/Photos (2)/maradona.jpg",
+    sanya: "/DvorFC/Photos (2)/sanya.jpg",
+    messi: "/DvorFC/Photos (2)/messi.jpg",
+    batrakov: "/DvorFC/Photos (2)/batrakov.jpg",
+    abibas: "/DvorFC/Photos (2)/abibas.jpg",
+    beloszhneka: "/DvorFC/Photos (2)/belosnezhka.jpg",
+    ramos: "/DvorFC/Photos (2)/ramos.jpg",
+    andryushka: "/DvorFC/Photos (2)/andryushka.jpg",
+    hokkeist: "/DvorFC/Photos (2)/hokkeist.jpg"
 };
-
-// ================================================================
