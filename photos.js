20ab3fd0680f9874b18c9d57aca6dc4a27223121
Ivan1421 <shustik142@gmail.com>
2026-06-28 17:45:14 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -22,7 +22,8 @@ const playerPhotos = {
     holland: "Photos (2)/haaland.jpg",
     egor_vulf: "Photos (2)/egor_vulf.jpg",
     matvey:"Photos (2)/neymar1.jpg",
-    saveliy:"Photos (2)/vini.jpg"
+    saveliy:"Photos (2)/vini.jpg",
+    artem:"Photos (2)/artem.jpg"
 };
 
 // ==================== ОТНОСИТЕЛЬНЫЕ ПУТИ К ЛОГОТИПАМ КЛУБОВ ====================
