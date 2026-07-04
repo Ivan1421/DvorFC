a43e1eacf664a09b4cd152223be30d14e89cede3
Ivan1421 <shustik142@gmail.com>
2026-07-05 00:40:24 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -41,7 +41,7 @@ const clubPhotos = {
 // ==================== ОТНОСИТЕЛЬНЫЕ ПУТИ К ФОТО ПЛЕЙСТАЙЛОВ ====================
 
 const playstylePhotos = {
-    jockey: "Photos (2)/jockey.png",
+    strag: "Photos (2)/strag.png",
     Aerial_Fortress: "Photos (2)/Aerial Fortress.png",
     enot: "Photos (2)/enot.png",
     Bezjalostniy:"Photos (2)/vinos_gold.png",
