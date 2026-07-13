f430f79eae421156d5121d6b1645f0bb8bbfc954
Ivan1421 <shustik142@gmail.com>
2026-07-13 14:02:36 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -59,7 +59,7 @@ const playstylePhotos = {
     parashut: "./Photos (2)/parashut.png",
     parashut_silver: "./Photos (2)/parashut_silver.png",
     tiki_taka: "./Photos (2)/tiki-taka.png",
-     tiki_taka_silver: "./Photos (2)/tiki-taka_silver.png",
+    tiki_taka_silver: "./Photos (2)/tiki-taka_silver.png",
     simulant_silver: "./Photos (2)/simulant_silver.png",
     pevec: "./Photos (2)/pevec.png"
 };
