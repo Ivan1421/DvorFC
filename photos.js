170fbebca742fba9d69112358b06c0a920790f1a
Ivan1421 <shustik142@gmail.com>
2026-07-28 21:16:08 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -27,7 +27,8 @@ const playerPhotos = {
     tankov: "./Photos%20(2)/DC2EE9E5-C247-4920-8D56-02213ECDEB6D.webp",
     sanya_bobrikov: "./Photos%20(2)/0C4CE6E9-38D5-4C38-9E92-B7E68933B6B2.webp",
     russkov_artem: "Russkov.jpg",
-    shuklin: "shuklin.jpg"
+    shuklin: "shuklin.jpg",
+    bombar: "bombar.jpg"
 };
 
 // ==================== ОТНОСИТЕЛЬНЫЕ ПУТИ К ЛОГОТИПАМ КЛУБОВ ====================
