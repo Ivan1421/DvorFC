58f70db59ff878e0dbb30830b775c661e7214a14
Ivan1421 <shustik142@gmail.com>
2026-07-28 20:49:18 +0700

    Update photos.js

--- photos.js
+++ photos.js
@@ -25,7 +25,8 @@ const playerPhotos = {
     timofey: "./Photos%20(2)/F8BE5DFF-01B2-478A-A49D-F3BDE1CE5841.webp",
     vanya: "./Photos%20(2)/kepa.jpg",
     tankov: "./Photos%20(2)/DC2EE9E5-C247-4920-8D56-02213ECDEB6D.webp",
-    sanya_bobrikov: "./Photos%20(2)/0C4CE6E9-38D5-4C38-9E92-B7E68933B6B2.webp"
+    sanya_bobrikov: "./Photos%20(2)/0C4CE6E9-38D5-4C38-9E92-B7E68933B6B2.webp",
+    russkov_artem: "russkov.jpg"
 };
 
 // ==================== ОТНОСИТЕЛЬНЫЕ ПУТИ К ЛОГОТИПАМ КЛУБОВ ====================
